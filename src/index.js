20086f9ef7aace1529db30d3655487ba32b1ea80
johnlindquist <johnlindquist@gmail.com>
2018-05-01 14:00:16 -0600

    separating components

--- src/index.js
+++ src/index.js
@@ -7,6 +7,79 @@ import "./index.css"
 // Get your own, free todos API 🙌 https://glitch.com/edit/#!/import/github/johnlindquist/todos-api
 const endpoint = "https://silly-boar.glitch.me/todos"
 
+const AddTodoForm = ({ addTodo, setTodo, current }) => (
+  <form onSubmit={addTodo}>
+    <input type="text" onChange={setTodo} value={current} />
+    <input type="submit" value="Add Todo" />
+  </form>
+)
+
+const EditTodo = ({ todo, update, toggleEdit, patchTodo }) => (
+  <form
+    style={{
+      display: "flex",
+      width: "50%"
+    }}
+    onSubmit={e => {
+      toggleEdit(e)
+      patchTodo(todo)
+    }}
+  >
+    <input
+      type="text"
+      value={todo.text}
+      onChange={update}
+      onBlur={toggleEdit}
+      autoFocus
+      style={{ flex: 1 }}
+    />
+    <input type="submit" />
+  </form>
+)
+
+const DisplayTodo = ({ todo, toggleDone, toggleEdit, deleteTodo }) => (
+  <div
+    style={{
+      display: "flex",
+      width: "50%"
+    }}
+  >
+    <span
+      style={{
+        flex: 1,
+        textDecoration: todo.done ? "line-through" : null
+      }}
+      onClick={toggleEdit}
+    >
+      {todo.text}
+    </span>
+    <button onClick={e => toggleDone(todo)}>Done</button>
+    <button onClick={e => deleteTodo(todo)}>X</button>
+  </div>
+)
+
+const Todo = ({
+  todo,
+  update,
+  isEditing,
+  toggleEdit,
+  patchTodo,
+  toggleDone,
+  deleteTodo
+}) => {
+  const editTodoProps = { todo, update, toggleEdit, patchTodo }
+  const displayTodoProps = { todo, toggleDone, toggleEdit, deleteTodo }
+  return (
+    <li>
+      {isEditing ? (
+        <EditTodo {...editTodoProps} />
+      ) : (
+        <DisplayTodo {...displayTodoProps} />
+      )}
+    </li>
+  )
+}
+
 const App = () => (
   <TodosStream endpoint={endpoint}>
     {({
@@ -17,70 +90,27 @@ const App = () => (
       toggleDone,
       deleteTodo,
       patchTodo
-    }) => {
-      return (
-        <div style={{ padding: "2rem" }}>
-          <form onSubmit={addTodo}>
-            <input type="text" onChange={setTodo} value={current} />
-            <input type="submit" value="Add Todo" />
-          </form>
-          <h3>Click a Todo to Edit</h3>
-          <ul>
-            {todos.map(todo => (
-              <TodoStream key={todo.id} todo={todo}>
-                {({ todo, update, isEditing, toggleEdit }) => {
-                  return (
-                    <li>
-                      {isEditing ? (
-                        <form
-                          style={{
-                            display: "flex",
-                            width: "50%"
-                          }}
-                          onSubmit={e => {
-                            toggleEdit(e)
-                            patchTodo(todo)
-                          }}
-                        >
-                          <input
-                            type="text"
-                            value={todo.text}
-                            onChange={update}
-                            onBlur={toggleEdit}
-                            autoFocus
-                            style={{ flex: 1 }}
-                          />
-                          <input type="submit" />
-                        </form>
-                      ) : (
-                        <div
-                          style={{
-                            display: "flex",
-                            width: "50%"
-                          }}
-                        >
-                          <span
-                            style={{
-                              flex: 1,
-                              textDecoration: todo.done ? "line-through" : null
-                            }}
-                            onClick={toggleEdit}
-                          >
-                            {todo.text}
-                          </span>
-                          <button onClick={e => toggleDone(todo)}>Done</button>
-                          <button onClick={e => deleteTodo(todo)}>X</button>
-                        </div>
-                      )}
-                    </li>
-                  )
-                }}
-              </TodoStream>
-            ))}
-          </ul>
-        </div>
-      )
-    }}
+    }) => (
+      <div style={{ padding: "2rem" }}>
+        <AddTodoForm addTodo={addTodo} setTodo={setTodo} current={current} />
+        <h3>Click a Todo to Edit</h3>
+        <ul>
+          {todos.map(todo => (
+            <TodoStream key={todo.id} todo={todo}>
+              {props => {
+                const todoProps = {
+                  ...props,
+                  toggleDone,
+                  deleteTodo,
+                  patchTodo
+                }
+                return <Todo {...todoProps} />
+              }}
+            </TodoStream>
+          ))}
+        </ul>
+      </div>
+    )}
   </TodosStream>
 )
 
